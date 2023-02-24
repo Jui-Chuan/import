4a1a19e92b4217709dd4b6fc6e5d73c14a736ed1
Sabrina Chang <sabrina.chang1019@gmail.com>
2023-02-24 17:51:02 +0800

    add count

--- import.py
+++ import.py
@@ -1,14 +1,15 @@
 import random
 r = random.randint(1,100)     #r是隨機數
+count = 0
 while True :
+	count = count + 1
 	number = input ('請猜一個數字:')
 	number = int (number)
 	if r == number :
 		print ('終於答對了')
 		break
 	elif r < number :
-		 print ('比答案大')
-					
+		 print ('比答案大')			
 	elif r > number :
 		 print ('比答案小')
-			
+	print ('你已經猜了', count , '次')
